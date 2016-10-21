68c8fba966ef2d1c1c6cfa0f6545af8830cd04c2
Daniel Weser <dweser@asu.edu>
2016-10-20 21:38:32 -0700

    Updated python wrapper and README

--- README.md
+++ README.md
@@ -3,35 +3,58 @@ PedestrianDM
 =========================
 
 **Table of Contents**
-- [Installation](#installation)
+- [Program requirements](#program-requirements)
 - [Program execution](#program-execution)
 - [Output](#output)
 - [Parameters](#parameters)
 - [The program](#the-program)
 
-## Installation
 
-From the main folder `PedestrianDM`, execute the `makefile`:
-```bash
-	> make
-```
-The compiler will create the executable file `PedestrianDM` in the directory `bin`. By default, the makefile uses the *g++* compiler with MacOS version detection to avoid Clang.
+## Program requirements
+
+The user must ensure the following Python packages are installed: 
+* NumPy, if the user wishes to plot or convert the data produced by the simulation; 
+* PyEVTK, if the user wishes to convert the data produced by the simulation to VTK format;
+* Matplotlib, if the user wishes to plot the data produced by the simulation.
 
 ## Program execution
 
-To execute the program, run the binary file:
+To execute the program, run the Python script in the main folder `PedestrianDM`:
 ```bash
-	> cd bin/
-	> ./PedestrianDM
+	> python main_pedestrian.py arg1 arg2 arg3 arg4 arg5
 ```
-The program reads the XML file `parameters/parameters.xml` and executes the program following those instructions. See the section [Parameters](#parameters) below.
 
-NOTE: Since the parameters are written in XML, white space is NOT ignored, e.g. `<IC>lattice</IC>` is not the same value as `<IC>  lattice  <\IC>`. While indentations and text-aligning looks cleaner, it should not be used in XML files.
+The arguments of the Python script are as follows:
+* `arg1` :  whether to run the simulation
+ * Valid values: 0, 1
+ * Default value: 1
+* `arg2` :  whether to convert the binary data in the folder `data` to VTK format and store in the folder `visualization/dataVTK`
+ * Valid values: 0, 1
+ * Default value: 0
+* `arg3` :  whether to plot the data in the folder `data` natively in Python
+ * Valid values: 0, 1
+ * Default value: 1
+* `arg4` :  how many periods of data to systematically skip in conversion or plotting
+ * Valid values: any positive integer
+ * Default value: 1
+* `arg5` :  whether to keep the binary data in the folder `data` after execution of the script
+ * Valid values: 0, 1
+ * Default value: 1
 
-During execution, the program  `PedestrianDM` will compute the positions
-(`X`) and the strategies (`S`) of the particles at each time step. In the
-terminal, the program will display a progress bar and, upon completion, 
-execution time.
+First, if `arg1=1`, then the program checks if the C++ source code has been compiled. 
+If not, it compiles the code for the user. Then, it executes the C++ executable `pedestrianDM` located in `bin`.
+First, the program reads the XML file `parameters/parameters.xml` and executes the program following those 
+instructions. See the section [Parameters](#parameters) below. Then, the program computes the positions
+(`X`) and the strategies (`S`) of the particles at each time step. Other than final execution time,
+nothing will be output to the terminal.
+
+Second, if `arg2=1`, then the program converts the binary data as indicated above, ommiting every `arg4` data files.
+
+Third, if `arg3=1`, then the program plots the data natively in Python, ommiting every `arg4` data files. 
+The data is plot as an animation, so the user must close the resulting window to stop the animation.
+
+Finally, if `arg5=1`, then the program deletes the binary data in the folder `data`.
+
 
 ## Output
 
@@ -69,6 +92,8 @@ where the value `******` is a counter of the time step.
 For the initial conditions (`IC`), `lattice` creates a lattice where the defective players are scattered randomly, `block` creates a lattice where the first n players are defective, and `uniform` scatters the players randomly across the domain with random cooperative/defective states.
 
 For the decision model (`DECISION_MODEL`), the valid options are as follows: `linear vm` - classic linear voter model, `norm vm` - linear voter model with normalized rates, `threshold vm` - threshold voter model, `br` - best response dynamics, and 'ising' - Ising model.
+
+NOTE: Since the parameters are written in XML, white space is NOT ignored, e.g. `<IC>lattice</IC>` is not the same value as `<IC>  lattice  <\IC>`. While indentations and text-aligning looks cleaner, it should not be used in XML files.
 
 ## The program
 
--- main_pedestrian.py
+++ main_pedestrian.py
@@ -2,24 +2,31 @@
 #!/usr/bin/python2.7
 
 
-from   pyevtk.hl import pointsToVTK
-import numpy as np
-import os 
-import os.path
-import time
-import xml.etree.ElementTree as ET
-import subprocess
-import sys
-import pygtk
-import matplotlib
-matplotlib.use('GTKAgg')
-import matplotlib.pyplot as plt
-from   matplotlib.patches import Ellipse
+# run simulation
+def runSimulation():
+	# check if cpp code is compiled
+	if (not os.path.isfile("bin/pedestrianDM")):
+		print "Compiling..."
+		process_make  = subprocess.Popen("make", stdout=subprocess.PIPE)
+		output, error = process_make.communicate()
+		# check for compilation errors
+		if (error is not None):
+			sys.exit("Compilation failed. Error: " + error)
+		else:
+			print "Compilation complete."
 
-
+	# execute pedestrianDM cpp code
+	os.chdir('bin')
+	process_pedestriancpp = subprocess.Popen("./pedestrianDM", stdout=subprocess.PIPE)
+	output, error         = process_pedestriancpp.communicate()
+	os.chdir('..')
+	return error
 
 # functions to load binary
 def loadBinary(file_name):
+	# load module within function to avoid cross-platform issues
+	import numpy as np
+
 	file  = open(file_name, "rb") 
 	file.seek(0, os.SEEK_SET)
 	array = np.fromfile(file, dtype=np.float64)
@@ -27,27 +34,21 @@ def loadBinary(file_name):
 	return array
 
 def loadBinaryInt(file_name):
+	# load module within function to avoid cross-platform issues
+	import numpy as np
+	
 	file  = open(file_name, "rb") 
 	file.seek(0, os.SEEK_SET)
 	array = np.fromfile(file, dtype=np.bool_)
 	file.close()
 	return array
 
-# create dictionary of relevant parameter values
-def loadParameters():
-	tree  	   = ET.parse("parameters/parameters.xml")
-	root  	   = tree.getroot()
-	N 	  	   = int(root.find("N").text)
-	dt 		   = float(root.find("DT").text)
-	nTime 	   = int(float(root.find("TIME").text)/float(root.find("DT").text))
-	length     = int(root.find("LENGTH").text)
-	width      = int(root.find("WIDTH").text)
-	seed_no	   = root.find("SEED_NUMBER").text
-	parameters = {'N': N, 'dt': dt, 'nTime': nTime, 'length': length, 'width': width, 'seed': seed_no}
-	return parameters
-
 # convert data from binary to vtk format
 def convertData(parameters, jump_print, data_keep):
+	# load modules within function to avoid cross-platform issues
+	import numpy as np
+	from pyevtk.hl import pointsToVTK
+	
 	cwd       = os.getcwd()
 	path_data = cwd + "/data/"
 	path_vtk  = cwd + "/visualization/dataVTK/"
@@ -62,56 +63,58 @@ def convertData(parameters, jump_print, data_keep):
 	else:
 		os.chdir(path_vtk)
 		os.system("perl -e 'for(<*>){((stat)[9]<(unlink))}'")
-		os.chdir('..')
+		os.chdir('../..')
 
 	# create z-coordinates
 	z = np.zeros(N)
 
-	for k in range(0, parameters['nTime']-1, jump_print):
+	for k in xrange(0, parameters['nTime']-1, jump_print):
 		extension = seed_name + "_" + str(k).zfill(9) + ext
 		# load data
-		x         = loadBinary(   path_data + "ParticleX_" + extension)[0:N]
-		y         = loadBinary(   path_data + "ParticleY_" + extension)[0:N]   
+		x         = loadBinary(   path_data + "particleX_" + extension)[0:N]
+		y         = loadBinary(   path_data + "particleY_" + extension)[0:N]   
 		# multiply by 1 to implicitly cast bool array as int
-		s         = loadBinaryInt(path_data + "ParticleS_" + extension)[0:N]*1
+		s         = loadBinaryInt(path_data + "particleS_" + extension)[0:N]*1
 	
 		# write data as VTK
 		pointsToVTK(path_vtk + "particles_" + str(k).zfill(9), x, y, z, data = {"Coop_def" : s})
-		
-		##########
-		# debug file conversion by writing csv
-		# np.savetxt(folderVTK + "particles_" + str(k).zfill(9) + ".csv", np.column_stack( (x, y, z, s) ), delimiter=",", fmt='%.14f')
-		##########
 
-	# clear directory of binary data
-	if (data_keep == 0):
-		os.chdir(path_data)
-		os.system("perl -e 'for(<*>){((stat)[9]<(unlink))}'")
-		os.chdir('..')
-
-# sub functions for plots
-def init_scatter_plot(parameters, x, y, color=1):
-    myFig 	= plt.figure()
-    ax 		= plt.gca()
-    ellipse =  Ellipse(xy=(0, 0), width=8.0, height=3, edgecolor='olive', fc='y', lw=2, zorder=0)
-    ax.add_patch(ellipse)
-    pltScat = plt.scatter(x, y, s=100, alpha=0.6, c=color, vmin=0, vmax=1)  # c='g'
-    title 	= plt.title('Time t=' + '{:04.2f}'.format(0),
-                         horizontalalignment='center', fontsize=20)
-    plt.axis([-parameters['length'], 2*parameters['length'], -parameters['width'], 2*parameters['width']])
-    plt.axes().set_aspect('equal', 'box')
-    plt.xlabel(r'$x$', fontsize=25)
-    plt.ylabel(r'$y$', fontsize=25)
-    plt.draw()
-    return (pltScat, title)
-
-def update_scatter_plot(pltScat, title, x, y, time, color=1):
-    pltScat.set_offsets(np.transpose(np.vstack([x, y])))
-    title.set_text('Time t=' + '{:04.2f}'.format(time) + ', N=' + str(len(x)))
-    pltScat.set_array(color)
+# create dictionary of relevant parameter values
+def loadParameters():
+	tree  	   = ET.parse("parameters/parameters.xml")
+	root  	   = tree.getroot()
+	N 	  	   = int(root.find("N").text)
+	dt 		   = float(root.find("DT").text)
+	nTime 	   = int(float(root.find("TIME").text)/float(root.find("DT").text))
+	length     = int(root.find("LENGTH").text)
+	width      = int(root.find("WIDTH").text)
+	seed_no	   = root.find("SEED_NUMBER").text
+	parameters = {'N': N, 'dt': dt, 'nTime': nTime, 'length': length, 'width': width, 'seed': seed_no}
+	return parameters
 
 # plot data
-def plotData(parameters, jump_print, data_conv):
+def animateData(parameters, jump_print, data_conv):
+	# load module within function to avoid cross-platform issues
+	import matplotlib.pyplot     as plt
+	from   matplotlib.animation  import FuncAnimation
+
+	# define our update function for FuncAnimation
+	def update_scatter_plot(frame_number):
+		# load module within function to avoid cross-platform issues
+		import numpy as np
+		
+		extension = seed_name + "_" + str(frame_number*jump_print).zfill(9) + ext
+		# load data
+		x         = loadBinary(   path_data + "particleX_" + extension)[0:N]
+		y         = loadBinary(   path_data + "particleY_" + extension)[0:N]   
+		# multiply by 1 to implicitly cast bool array as int
+		s         = loadBinaryInt(path_data + "particleS_" + extension)[0:N]*1
+
+		plt_sct.set_offsets(np.transpose(np.vstack([x, y])))
+		title.set_text('Time t=' + '{:04.2f}'.format(frame_number*jump_print*dt))
+		plt_sct.set_array(s)
+
+    # parameters
 	cwd       = os.getcwd()
 	path_data = cwd + "/data/"
 	path_vtk  = cwd + "/visualization/dataVTK/"
@@ -120,92 +123,99 @@ def plotData(parameters, jump_print, data_conv):
 	dt 		  = parameters['dt']
 	ext 	  = ".dat"
 	
-	# draw first plot
+	## initialization of plot ##
 	extension = seed_name + "_" + str(0).zfill(9) + ext
-	# load data
-	x         = loadBinary(   path_data + "ParticleX_" + extension)[0:N]
-	y         = loadBinary(   path_data + "ParticleY_" + extension)[0:N]   
+	# load initial data
+	x         = loadBinary(   path_data + "particleX_" + extension)[0:N]
+	y         = loadBinary(   path_data + "particleY_" + extension)[0:N]   
 	# multiply by 1 to implicitly cast bool array as int
-	s         = loadBinaryInt(path_data + "ParticleS_" + extension)[0:N]*1
+	s         = loadBinaryInt(path_data + "particleS_" + extension)[0:N]*1
 
-	plt.ion()
+	# initial figure
+	fig 	= plt.figure()
+	ax 		= plt.gca()
+	plt_sct = plt.scatter(x, y, s=100, alpha=0.6, c=s, vmin=0, vmax=1)
+	title 	= plt.title('Time t=' + '{:04.2f}'.format(0), horizontalalignment='center', fontsize=20)
+	plt.axis([-parameters['length'], 2*parameters['length'], -parameters['width'], 2*parameters['width']])
+	plt.axes().set_aspect('equal', 'box')
+	plt.xlabel(r'$x$', fontsize=25)
+	plt.ylabel(r'$y$', fontsize=25)
+
+	# create animation and display it
+	animation = FuncAnimation(fig, update_scatter_plot, frames=parameters['nTime']/jump_print, interval=1)
 	plt.show()
-	pltScat, title = init_scatter_plot(parameters, x, y, s)
-
-	# plot rest of data
-	for t in range(1, parameters['nTime']-1, jump_print):
-		extension = seed_name + "_" + str(t).zfill(9) + ext
-		# load data
-		x         = loadBinary(   path_data + "ParticleX_" + extension)[0:N]
-		y         = loadBinary(   path_data + "ParticleY_" + extension)[0:N]   
-		# multiply by 1 to implicitly cast bool array as int
-		s         = loadBinaryInt(path_data + "ParticleS_" + extension)[0:N]*1
-
-		update_scatter_plot(pltScat, title, x, y, t*dt, s)
-		plt.draw()
-		time.sleep(.01)
 
 
 
 # main execution
 if __name__ == "__main__":
-	# check if cpp code is compiled
-	if (not os.path.isfile(".comp")):
-		process_make  = subprocess.Popen("make", stdout=subprocess.PIPE)
-		output, error = process_make.communicate()
-		# check for compilation errors
-		if (error is not None):
-			print "Compilation failed. Error: ", error
-		# make empty .comp file
-		open(".comp", 'a').close()
-
-	# execute pedestrianDM cpp code
-	tic = time.time()
-	os.chdir('bin')
-	process_pedestriancpp = subprocess.Popen("./pedestrianDM", stdout=subprocess.PIPE)
-	output, error         = process_pedestriancpp.communicate()
-	os.chdir('..')
-	toc = time.time() - tic
-
-	# check for errors
-	if (error is not None):
-		print "Simulation returned error message: ", error
-	else:
-		print "Simulation complete.", '\t\t', "Elapsed time: ", round(toc,2), " seconds."
+	import os 
+	import os.path
+	import time
+	import xml.etree.ElementTree as ET
+	import subprocess
+	import sys
 
 	# check if any arguments were passed to script
-	if (not sys.argv):
-		# default values are ones
-		data_conv  = 1
-		data_keep  = 1
-		jump_print = 1
+	# note: script name ('main_pedestrian.py') is 0th argument
+	if (len(sys.argv) == 1):
+		# default values
+		run_simu   = 1
+		data_conv  = 0
 		data_plot  = 1
-	# first input is whether to plot data
-	elif (len(sys.argv) == 1):
-		data_conv  = int(sys.argv[1])
-		data_keep  = 1
 		jump_print = 1
-		data_plot  = 1
-	# second input is whether to keep data after plotting
+		data_keep  = 1
+	# first input is whether to run simulation
 	elif (len(sys.argv) == 2):
-		data_conv  = int(sys.argv[1])
-		data_keep  = int(sys.argv[2])
+		run_simu   = int(sys.argv[1])
+		data_conv  = 0
+		data_plot  = 1
 		jump_print = 1
+		data_keep  = 1
+	# second input is whether to convert data
+	elif (len(sys.argv) == 2):
+		run_simu   = int(sys.argv[1])
+		data_conv  = int(sys.argv[2])
 		data_plot  = 1
-	# third input is number of frames to skip
+		jump_print = 1
+		data_keep  = 1
+	# third input is whether to plot data
 	elif (len(sys.argv) == 3):
-		data_conv  = int(sys.argv[1])
-		data_keep  = int(sys.argv[2])
-		jump_print = int(sys.argv[3])
-		data_plot  = 1
+		run_simu   = int(sys.argv[1])
+		data_conv  = int(sys.argv[2])
+		data_plot  = int(sys.argv[3])
+		jump_print = 1
+		data_keep  = 1
+	# fourth input is number of frames to skip during data conversion and plotting
+	elif (len(sys.argv) == 4):
+		run_simu   = int(sys.argv[1])
+		data_conv  = int(sys.argv[2])
+		data_plot  = int(sys.argv[3])
+		jump_print = int(sys.argv[4])
+		data_keep  = 1
+	# fifth input is whether to keep data after plotting
 	else:
-		data_conv  = int(sys.argv[1])
-		data_keep  = int(sys.argv[2])
-		jump_print = int(sys.argv[3])
-		data_plot  = int(sys.argv[4])
+		run_simu   = int(sys.argv[1])
+		data_conv  = int(sys.argv[2])
+		data_plot  = int(sys.argv[3])
+		jump_print = int(sys.argv[4])
+		data_keep  = int(sys.argv[5])
+
+	# run simulation
+	if (run_simu == 1):
+		print "Running simulation..."
+		tic = time.clock()
+		error = runSimulation()
+		toc = time.clock() - tic
+		# check for errors
+		if (error is not None):
+			sys.exit("Simulation returned error message: " + error)
+		else:
+			print "Simulation complete.", '\t\t', "Elapsed time: ", round(toc,2), " seconds."
 
 	# convert data from binary
 	if (data_conv == 1):
+		print "Converting data..."
 		tic = time.clock()
 		parameters = loadParameters()
 		convertData(parameters, jump_print, data_keep)
@@ -216,5 +226,15 @@ if __name__ == "__main__":
 	if (data_plot == 1):
 		if (data_conv == 0):
 			parameters = loadParameters()
-		plotData(parameters, jump_print, data_conv)
+		print "Plotting data..."
+		animateData(parameters, jump_print, data_conv)
+		print "Plotting complete."
+
+	# clear directory of binary data
+	if (data_keep == 0):
+		print "Deleting binary data..."
+		os.chdir(path_data)
+		os.system("perl -e 'for(<*>){((stat)[9]<(unlink))}'")
+		os.chdir('..')
+		print "Binary data deletion complete."
 
